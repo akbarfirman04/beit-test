7c9bca1ad67d4c25c21db600c3a2467de6d42d5f
akbarfirman04 <akbarfirman04@gmail.com>
2023-08-05 12:38:47 +0700

    first

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <span>tes</span>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'IndexPage',
+  data: () => ({
+    kelas1: [],
+    kelas2: [],
+    kelas3: [],
+    kelas4: [],
+    kelas5: [],
+    kelas6: [],
+  }),
+  created() {
+    this.getData()
+  },
+  methods: {
+    async getData() {
+      await axios({
+        method: 'get',
+        url: 'http://ecocim-backend-theone.beit.co.id/api/ManualConfig/TestBEIT',
+      }).then(function (response) {
+        console.log(response.data)
+      })
+    },
+  },
+}
+</script>
